ce7142d8e0491bae49113e930fcf9eba99835fa0
sanjay <sanjaykumar73189@gmail.com>
2022-09-03 10:08:27 +0530

    nothing

--- canConstruct.js
+++ canConstruct.js
@@ -1,23 +1,21 @@
-const canconstruct=(target, words, memo={})=>{
-    if(target in memo) return memo[target]
-if(target=='') return true
+const canconstruct = (target, words, memo = {}) => {
+  if (target in memo) return memo[target];
+  if (target == "") return true;
 
-    for(let word of words){
-        if(target.indexOf(word)===0){
-            const suffix=target.slice(word.length)
-            console.log(suffix)
-            if(canconstruct(suffix, words,memo)===true){
-                memo[suffix]=true;
-                return true;
-            };
-        }
+  for (let word of words) {
+    if (target.indexOf(word) === 0) {
+      const suffix = target.slice(word.length);
+      console.log(suffix);
+      if (canconstruct(suffix, words, memo) === true) {
+        memo[suffix] = true;
+        return true;
+      }
     }
+  }
 
-memo[target]=false;
-return false;
-}
+  memo[target] = false;
+  return false;
+};
 
-
-
-console.log(canconstruct("abcdef",["ab", "abc", "cd","def","abcd"]))
+console.log(canconstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
 // console.log(canconstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",["e", "eeeeee", "eeeeeeeeeeeeeee","eeeeeeeeeeeeeeeeeeeeee","eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"]))
--- howConstruct.js
+++ howConstruct.js
@@ -1,23 +1,31 @@
-const countConstruct=(target, words, memo={})=>{
-   if(target in memo ) return memo[target]
-if(target=='') return 1
+const countConstruct = (target, words, memo = {}) => {
+  if (target in memo) return memo[target];
+  if (target == "") return 1;
 
-    let totalsum=0
-    for(let word of words){
-        if(target.indexOf(word)===0){
-            const suffix=target.slice(word.length)
-            const result=countConstruct(suffix, words,memo)
+  let totalsum = 0;
+  for (let word of words) {
+    if (target.indexOf(word) === 0) {
+      const suffix = target.slice(word.length);
+      const result = countConstruct(suffix, words, memo);
 
-            totalsum=totalsum+result;
-    
-        }
+      totalsum = totalsum + result;
     }
+  }
 
-memo[target]=totalsum
-return totalsum;
-}
+  memo[target] = totalsum;
+  return totalsum;
+};
 
-
-
-console.log(countConstruct("abcdef",["ab", "abc", "cd","def","abcd"]))
-console.log(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",["e", "eeeeee", "eeeeeeeeeeeeeee","eeeeeeeeeeeeeeeeeeeeee","eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef"]))
+console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+console.log(
+  countConstruct(
+    "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+    [
+      "e",
+      "eeeeee",
+      "eeeeeeeeeeeeeee",
+      "eeeeeeeeeeeeeeeeeeeeee",
+      "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+    ]
+  )
+);
